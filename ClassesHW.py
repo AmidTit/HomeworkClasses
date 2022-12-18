3932732ea45f4ae0b4a1cb6711cf46676b216e0e
Dmitry Titiaev <dimalordik@gmail.com>
2022-12-18 16:41:29 +0300

    __str__, __lt__ 'overload'

--- ClassesHW.py
+++ ClassesHW.py
@@ -11,14 +11,30 @@ class Student:
         self.finished_courses.append(course_name) 
 
     def rate_lecturer(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in self.lectures_attached and course in self.courses_in_progress:
+        if isinstance(lecturer, Lecturer) and course in lecturer.lectures_attached and course in self.courses_in_progress:
             if course in lecturer.lecturer_grade:
                 lecturer.lecturer_grade[course] += [grade]
             else:
                 lecturer.lecturer_grade[course] = [grade]
         else:
             return 'Error'
-        pass   
+
+    def _average_homework_grade(self):
+        res = sum(*self.grades.value()) / len(*self.grades.values())
+        return res
+
+    def __str__(self):
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\n'
+        f'Средняя оценка за домашние задания: {self._average_homework_grade()}\n'
+        f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n'
+        f'Завершенные курсы: {", ".join(self.finished_courses)}'
+        return res
+
+    def __lt___(self, other):
+        if not isinstance (other, Student):
+            print("Not a Student!")
+            return
+        return self._average_homework_grade() < other._average_homework_grade()    
 
 class Mentor:
     def __init__(self, name, surname):
@@ -29,6 +45,22 @@ class Mentor:
 class Lecturer(Mentor):
     lectures_attached = []
     lecturer_grade = {}
+
+    def _average_lectures_grade(self):
+        result = sum(*self.lecturer_grade.values()) / len(*self.lecturer_grade.values())
+        return result
+
+    def __str__(self):
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self._average_lectures_grade()}'    
+        return res
+
+    def __lt___(self, other):
+        if not isinstance (other, Lecturer):
+            print("Not a Lecturer")
+            return
+        return self._average_lectures_grade() < other._average_lectures_grade()    
+
+
     
 
 class Reviewer(Mentor):
@@ -41,15 +73,13 @@ class Reviewer(Mentor):
         else:
             return 'Error'
 
+
+    def __str__(self):
+        res = f'Имя: {self.name}\nФамилия: {self.surname}'
+        return res
+
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
  
-
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
- 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
- 
-print(best_student.grades)
+dmitry_titiaev = Reviewer('Dmitry', 'Titiaev')
+print (dmitry_titiaev)
